72acd38e02bb9ca647039494f7ed7eec725d6968
pstepien-design <pstepien@interia.pl>
2021-12-20 18:32:21 +0100

    adding cors

--- src/main/java/com/example/demo/Configuration/WebSecurityConfigurator.java
+++ src/main/java/com/example/demo/Configuration/WebSecurityConfigurator.java
@@ -1,35 +1,20 @@
-package com.example.demo.Configuration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebSecurityConfigurator extends WebSecurityConfigurerAdapter {
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+public class WebSecurityConfigurator {
 
-        configuration.setAllowedOrigins(
-                Arrays.asList("http://localhost:5500",  "http://127.0.0.1:5500")
-        );
-        configuration.setAllowedMethods(
-                Arrays.asList("GET", "POST", "PATCH", "DELETE", "PUT")
-        );
-        configuration.setAllowedHeaders(
-                Arrays.asList(
-                        "Authorization",
-                        "Content-Type",
-                        "Access-Control-Allow-Origin"
-                )
-        );
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
