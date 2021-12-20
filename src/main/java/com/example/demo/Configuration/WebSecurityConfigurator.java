f94f0febf41380829cf6f640e43bc573b5c097b3
pstepien-design <pstepien@interia.pl>
2021-12-20 18:10:46 +0100

    adding cors

---
+++ src/main/java/com/example/demo/Configuration/WebSecurityConfigurator.java
@@ -0,0 +1,35 @@
+package com.example.demo.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebSecurityConfigurator extends WebSecurityConfigurerAdapter {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://localhost:5500")
+        );
+        configuration.setAllowedMethods(
+                Arrays.asList("GET", "POST", "PATCH", "DELETE", "PUT")
+        );
+        configuration.setAllowedHeaders(
+                Arrays.asList(
+                        "Authorization",
+                        "Content-Type",
+                        "Access-Control-Allow-Origin"
+                )
+        );
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
